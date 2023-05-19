4c03ac1461fd38ac82d8f839056ced7c1a4430f9
abhilasha.garkoti <abhilasha.garkoti@gmail.com>
2023-05-19 18:01:38 +0530

    comitfirst

---
+++ index.js
@@ -0,0 +1,23 @@
+function addition() {
+    let a = parseInt(prompt("enter the number"));
+    let b = parseInt(prompt("enter the number"));
+    document.write(a + b, "<br>");
+
+}
+function subtraction() {
+    let a = parseInt(prompt("enter the number"));
+    let b = parseInt(prompt("enter the number"));
+    document.write(a - b, "<br>");
+}
+function multiply() {
+    let a = parseInt(prompt("enter the number"));
+    let b = parseInt(prompt("enter the number"));
+    document.write(a * b, "<br>");
+}
+function divide() {
+    let a = parseInt(prompt("enter the number"));
+    let b = parseInt(prompt("enter the number"));
+    document.write(a / b, "<br>");
+}
+
+
